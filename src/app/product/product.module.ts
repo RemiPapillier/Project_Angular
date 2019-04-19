1694cde5b36ff0106f3d8bade09e0737ab8a684f
Remi Papillier <remi-papillier@hotmail.fr>
2019-04-19 10:44:06 +0200

    details

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
-  { path: '', component: ListComponent }
+  { path: '', component: ListComponent },
+  { path: ':id', component: DetailComponent },
 ];
 
 @NgModule({
@@ -13,7 +15,8 @@ const routes: Routes = [
     RouterModule.forChild(routes),
   ],
   declarations: [
-    ListComponent
+    ListComponent,
+    DetailComponent
   ],
   exports: [
   ]
